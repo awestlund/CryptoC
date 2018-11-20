adc0f6e53612958523886cdd1d983d7e94d8012f
Amber Westlund <awestlund@wisc.edu>
2018-11-20 10:18:13 -0500

    init

---
+++ crytp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// init with in the folder/console dotnet new console -o someapp
+namespace crytp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
